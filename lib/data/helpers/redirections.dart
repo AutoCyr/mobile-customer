76f33d024174576c381da3614162f4f6b86ad94a
Ginott Log <ginlogossou@gmail.com>
2025-06-12 11:18:48 +0000

    Config url launcher

--- lib/data/helpers/redirections.dart
+++ lib/data/helpers/redirections.dart
@@ -9,58 +9,83 @@ import 'package:url_launcher/url_launcher.dart';
 class Redirections {
 
   launchWebsite({required BuildContext context, required String url}) async {
-    Uri uri = Uri.parse(url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri, mode: LaunchMode.externalApplication);
-    } else {
-      Snacks.failureBar("Le lien n'est pas valide", context);
+    try {
+      Uri uri = Uri.parse(url);
+      if (await canLaunchUrl(uri)) {
+        await launchUrl(uri, mode: LaunchMode.externalApplication);
+      } else {
+        Snacks.failureBar("Le lien n'est pas valide", context);
+      }
+    } catch (e, stackTrace) {
+      print(e);
+      print(stackTrace);
     }
   }
 
   launchCall({required BuildContext context, required String number}) async {
-    String url = "tel://$number";
-    Uri uri = Uri.parse(url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri, mode: LaunchMode.externalApplication);
-    } else {
-      Snacks.failureBar("Le numéro n'est pas valide", context);
+    try {
+      String url = "tel://$number";
+      Uri uri = Uri.parse(url);
+      if (await canLaunchUrl(uri)) {
+        await launchUrl(uri, mode: LaunchMode.externalApplication);
+      } else {
+        Snacks.failureBar("Le numéro n'est pas valide", context);
+      }
+    } catch (e, stackTrace) {
+      print(e);
+      print(stackTrace);
     }
   }
 
   launchMail({required BuildContext context, required String email}) async {
-    String url = "mailto:$email";
-    Uri uri = Uri.parse(url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri, mode: LaunchMode.externalApplication);
-    } else {
-      Snacks.failureBar("L'adresse mail n'est pas valide", context);
+    try {
+      String url = "mailto:$email";
+      Uri uri = Uri.parse(url);
+      if (await canLaunchUrl(uri)) {
+        await launchUrl(uri, mode: LaunchMode.externalApplication);
+      } else {
+        Snacks.failureBar("L'adresse mail n'est pas valide", context);
+      }
+    } catch (e, stackTrace) {
+      print(e);
+      print(stackTrace);
     }
   }
 
   openMap({required BuildContext context, required String latitude, required String longitude}) async {
-    String googleMapUrl = 'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
+    try {
+      String googleMapUrl = 'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
 
-    if (await canLaunchUrl(Uri.parse(googleMapUrl))) {
-      await launchUrl(Uri.parse(googleMapUrl));
-    } else {
-      Snacks.failureBar("Les coordonnées géographiques ne sont pas valides", context);
+      if (await canLaunchUrl(Uri.parse(googleMapUrl))) {
+        await launchUrl(Uri.parse(googleMapUrl));
+      } else {
+        Snacks.failureBar("Les coordonnées géographiques ne sont pas valides", context);
+      }
+    } catch (e, stackTrace) {
+      print(e);
+      print(stackTrace);
     }
   }
 
   shareWhatsapp({required BuildContext context, required DetailPiece piece, required Partenaire partenaire}) async {
-    var message = "Bonjour ${partenaire.raisonSociale}. \n\n"
-        "Nous vous contactons depuis AUTOCYR par rapport à la pièce ${piece.piece != null ? piece.piece!.nomPiece : piece.article!.name}...";
+    try {
+      var message = "Bonjour ${partenaire.raisonSociale}. \n\n"
+          "Nous vous contactons depuis AUTOCYR par rapport à la pièce ${piece.piece != null ? piece.piece!.nomPiece : piece.article!.name}...";
 
-    var AndroidUrl = "whatsapp://send?phone=${partenaire.telephonePartenaire}&text=$message";
-    var iOSUrl ="https://wa.me/${partenaire.telephonePartenaire}?text=$message";
+      var AndroidUrl = "whatsapp://send?phone=${partenaire.telephonePartenaire}&text=$message";
+      var iOSUrl ="https://wa.me/${partenaire.telephonePartenaire}?text=$message";
 
-    String url = Platform.isAndroid ? AndroidUrl : iOSUrl;
-    Uri uri = Uri.parse(url);
+      String url = Platform.isAndroid ? AndroidUrl : iOSUrl;
+      Uri uri = Uri.parse(url);
 
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri, mode: LaunchMode.externalApplication);
-    } else {
-      Snacks.failureBar("Le lien n'est pas valide", context);
+      if (await canLaunchUrl(uri)) {
+        await launchUrl(uri, mode: LaunchMode.externalApplication);
+      } else {
+        Snacks.failureBar("Le lien n'est pas valide", context);
+      }
+    } catch (e, stackTrace) {
+      print(e);
+      print(stackTrace);
     }
   }
 
